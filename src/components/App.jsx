a15074344cfeb4d9634e5793b23267430769f8d5
pmasharov <2386444@gmail.com>
2019-03-11 21:11:18 +0300

    add skills, education, experience, portfolio, contacts components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import Avatar from './Avatar';
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import NavPanel from './NavPanel';
+import Avatar from './Avatar';
+import Skills from './Skills';
+import Education from './Education';
+import Experience from './Experience';
+import Portfolio from './Portfolio';
+import Contacts from './Contacts';
 
 class App extends Component {
     render() {
@@ -8,6 +14,15 @@ class App extends Component {
             <div>
                 <Avatar />
                 <NavPanel />
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/skills" component={Skills} />
+                        <Route path="/education" component={Education} />
+                        <Route path="/experience" component={Experience} />
+                        <Route path="/portfolio" component={Portfolio} />
+                        <Route path="/contacts" component={Contacts} />
+                    </Switch>
+                </BrowserRouter>
             </div>
         )
     }
